514abc412083850eda42877f6ffcc2661b898503
YUCHENGAA <aacheng7799@gmail.com>
2026-06-08 14:59:27 +0800

    安裝quasar套件

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,7 @@
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
+  <q-btn color="primary" label="Hello Quasar" />
 </template>
 
 <style scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,15 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import { Quasar } from 'quasar'
+import 'quasar/src/css/index.sass'
+
+import '@quasar/extras/material-icons/material-icons.css'
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(Quasar)
 
 app.mount('#app')
